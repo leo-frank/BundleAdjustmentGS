94425e0a2d25615b857e33a4b7788bf618704ae8
Wynn <1052209675@qq.com>
2024-10-14 17:09:15 +0800

    revise save_iterations; revise camera model

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -81,8 +81,7 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
         uid = intr.id
         R = np.transpose(qvec2rotmat(extr.qvec))
         T = np.array(extr.tvec)
-
-        if intr.model=="SIMPLE_PINHOLE":
+        if intr.model=="SIMPLE_PINHOLE" or intr.model=="SIMPLE_RADIAL":
             focal_length_x = intr.params[0]
             FovY = focal2fov(focal_length_x, height)
             FovX = focal2fov(focal_length_x, width)
